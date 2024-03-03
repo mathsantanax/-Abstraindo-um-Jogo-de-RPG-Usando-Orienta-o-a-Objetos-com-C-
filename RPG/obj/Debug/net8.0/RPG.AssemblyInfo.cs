bbed18496016334639ccbecf4e20b9f5f0a7e8fd
mathsantanax <matheus.santana7@hotmail.com>
2024-03-03 18:19:29 -0300

    fix

--- RPG/obj/Debug/net8.0/RPG.AssemblyInfo.cs
+++ RPG/obj/Debug/net8.0/RPG.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d8068c0ce5a4402a04b03cf5294d14cf813be39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+075cd2f78637277ddc9873d89447648772105cb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
